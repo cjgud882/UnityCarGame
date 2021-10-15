9d54c3c6d0dfcc2510766cd2b61ed0dd2cb9a72d
Nathan <nrvan693@learner.dvusd.org>
2021-10-15 08:36:05 -0700

    Working on Car Selection Menu. It's partly done, this is for downloading on my home computer.

---
+++ CarGame/Assets/_Scripts/_carSelection/_characterManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class _characterManager : MonoBehaviour
+{
+    public _characterDatabase characterDB;
+    
+    public Text nameText;
+    public SpriteRenderer artworkSprite;
+
+    private int selectedOption = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void NextOption()
+    {
+        selectedOption++;
+        
+        if (selectedOption >= characterDB.characterCount)
+        {
+            selectedOption = 0;
+        }
+    }
+
+    private void UpdateCharacter(int selectedOption)
+    {
+        _character character = characterDB.GetCharacter(selectedOption);
+        artworkSprite.sprite = character.characterSprite;
+        nameText.text = character.characterName;
+    }
+}
