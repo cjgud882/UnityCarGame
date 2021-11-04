33ea89deb34f1210fc2b090fe9163b9f0b3a119c
Caden <cjgud882@leaner.dvusd.org>
2021-11-04 08:53:13 -0700

    Main Menu Testing Functionality
    - Main menu buttons do stuff now

---
+++ CarGame/Assets/_Scripts/_Enemy/enemyDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyDamage : MonoBehaviour
+{
+    //put this script on the enemy
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Rocket")
+        {
+            Debug.Log("BOOOOOOM");
+
+        }
+    }
+
+
+
+}
