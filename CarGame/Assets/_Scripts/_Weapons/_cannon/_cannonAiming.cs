09cfca60361194e61d4d6226e3a4260c1d546b6e
Caden <cjgud882@leaner.dvusd.org>
2021-11-08 09:02:30 -0700

    pieced a bit together
    cannon still buggy af

--- CarGame/Assets/_Scripts/_Weapons/_cannon/_cannonAiming.cs
+++ CarGame/Assets/_Scripts/_Weapons/_cannon/_cannonAiming.cs
@@ -7,6 +7,8 @@ public class _cannonAiming : MonoBehaviour
     public float moveSpeed = 5;
     public Rigidbody2D rb;
     public Camera cam;
+    public Transform anchorPos;
+    public Transform cannonPos;
 
     Vector2 movement;
     Vector2 mousePos;
@@ -16,15 +18,19 @@ public class _cannonAiming : MonoBehaviour
     {
         // moves cam to where mouse is pointing
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        
     }
-
+    
     void FixedUpdate()
     {
+        cannonPos.position = anchorPos.position;
         // moves cursor to where mouse is pointing (aiming)
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
         rb.rotation = angle;
+
+        cannonPos.position = anchorPos.position;
     }
 }
