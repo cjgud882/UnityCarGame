2ebba17ec2601c1673c63f4753bba3087078ba25
Nathan <nrvan693@learner.dvusd.org>
2021-11-09 09:16:53 -0700

    Deleted useless scene and fixed load and save system

---
+++ CarGame/Assets/_Scripts/_Car/_player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _player : MonoBehaviour
+{
+    public _characterDatabase characterDB;
+
+    public SpriteRenderer artworkSprite;
+
+    private int selectedOption = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("selectedOption"))
+        {
+            selectedOption = 0;
+        }
+        else
+        {
+            Load();
+        }
+
+        UpdateCharacter(selectedOption);
+    }
+
+    private void UpdateCharacter(int selectedOption)
+    {
+        _character character = characterDB.GetCharacter(selectedOption);
+        artworkSprite.sprite = character.characterSprite;
+    }
+
+    private void Load()
+    {
+        selectedOption = PlayerPrefs.GetInt("selectedOption");
+    }
+}
